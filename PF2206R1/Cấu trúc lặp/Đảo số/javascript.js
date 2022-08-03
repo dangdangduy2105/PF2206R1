3841f4dea60854761f5558b3ab36205f6a447cd5
dangdangduy2105 <duya221052000>
2022-08-04 01:05:10 +0700

    bai tap dao so

--- PF2206R1/Cấu trúc lặp/Đảo số/javascript.js
+++ PF2206R1/Cấu trúc lặp/Đảo số/javascript.js
@@ -1,10 +1,18 @@
-let inputNumber = 987654321;
-let st = 0;
-let result = 0;
+function reverse(){
+  let inputNumber = document.getElementById('number').value;
+  let lastDigit = 0;
+  let result = 0;
+  let output = document.getElementById('output')
+  
+  do{
+    lastDigit = parseInt(inputNumber%10);
+    result = parseInt(result*10 + lastDigit);
+    inputNumber = parseInt(inputNumber/10);
+  }while(inputNumber > 0)
+  output.innerText = result;
+}
 
-do{
-  lastDigit = parseInt(inputNumber%10);
-  result = parseInt(result*10 + lastDigit);
-  inputNumber = parseInt(inputNumber/10);
-}while(inputNumber > 0)
-document.write(result)
+function cancel(){
+  document.getElementById('number').value = '';
+  document.getElementById('output').innerText = '';
+}
