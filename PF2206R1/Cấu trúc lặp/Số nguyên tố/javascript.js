32f0ad95c144c1bfe8359ca6407768364f5e27c6
dangdangduy2105 <duya221052000>
2022-08-04 15:09:47 +0700

    so nguyen to

--- PF2206R1/Cấu trúc lặp/Số nguyên tố/javascript.js
+++ PF2206R1/Cấu trúc lặp/Số nguyên tố/javascript.js
@@ -1,22 +1,22 @@
 function cal(){
   let num = parseInt(document.getElementById('num').value);
   let output = document.getElementById('output');
-  let fbnci = [0, 1];
-  let i = 1;
-  while(i < num) {
-    fbnci.push(fbnci[i] + fbnci[i-1]);
-    i++;
-  }
-  output.innerText = fbnci;
-}
+  let count = 0;
+  let result = [];
 
+  
+  for(let i = 0; i < num; i++){ 
+    if(i % 2 == 1){
+      result.push(i);
+      count++;
+    }else{
+      num++;
+    }
+  }  
+  output.innerText = result;
+}
 
 function cancel(){
   document.getElementById('num').value = '';
   document.getElementById('output').innerText = '';
 }
-// f1 = 1;
-// f2 = 1;
-// f3 = f1+f2; //1+1 = 2;
-// f4 = f3+f2; //2+1 = 3;
-// f5 = f4+f3; //3+2 = 5;
